1d854b1c4de9900901198cd2f3e8e0e3830bcf3c
ben <limhanyangb@gmail.com>
2017-10-22 19:06:57 -0500

    First commit

---
+++ main_code/main_code.ino
@@ -0,0 +1,13 @@
+//  My First Rocket
+//  Do not use delay!!! There might be other timing critical routines 
+
+void setup() {
+  // put your setup code here, to run once:
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
