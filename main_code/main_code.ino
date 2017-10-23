559a19e3628d6ef19b0276004cb2e50151a03652
ben <limhanyangb@gmail.com>
2017-10-22 19:07:48 -0500

    update

--- main_code/main_code.ino
+++ main_code/main_code.ino
@@ -1,5 +1,6 @@
 //  My First Rocket
-//  Do not use delay!!! There might be other timing critical routines 
+//  Do not use delay!!! There might be other timing critical routines
+//  To add your function, use a .h and a .cpp file  
 
 void setup() {
   // put your setup code here, to run once:
