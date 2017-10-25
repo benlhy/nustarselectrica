7635e90e5d34bb0ec50eba73f1400438d7d8d182
ben <limhanyangb@gmail.com>
2017-10-25 09:52:15 -0500

    Added display

--- main_code/main_code.ino
+++ main_code/main_code.ino
@@ -1,14 +1,49 @@
 //  My First Rocket
-//  Do not use delay!!! There might be other timing critical routines
-//  To add your function, use a .h and a .cpp file  
+#include 'display_header.h' 
 
 void setup() {
-  // put your setup code here, to run once:
-  
-
+  // some code to start
+  init_all();
+  //screen_huh(); // check if display is on or off
+  //spin_parameters_huh(); // ask ground station if we want to set new spin parameters
+  //motor_test_huh(); // do we want a motor test?
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // keep on spinning
 
 }
+
+
+// This function begins all functions
+void init_all() {
+  Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
+  display.display();
+  display.clearDisplay();
+  while(radio_init()){
+    dis(0,0,0,0,0);
+  }
+  // Okay radio initialized, tell ground that we are alive.
+  radio.send("Establishing comms");
+  while(motor_init()){
+    dis(1,0,0,0,0);
+  }
+  while(altitude_init()){
+    dis(1,1,0,0,0);
+  }
+  while(gps_init()){
+    dis(1,1,1,0,0);
+  }
+  while(accelometer_init()){
+    dis(1,1,1,1,0);
+  }
+  dis(1,1,1,1,1);
+  radio_send("Systems online");
+}
+
+// This function displays the status of the rocket
+void dis(int radio, int motor,int alt, int gps, int acc){
+  
+}
+
