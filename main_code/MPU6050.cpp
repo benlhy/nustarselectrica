0333babef2cde535767941f274d1192372160e2c
aaron-r-nelson <aaron-r-nelson@users.noreply.github.com>
2017-11-03 15:35:19 -0500

    Calculates time difference
    Improvement over assuming static frequency. Found millis() to be more accurate than micros(); still unsure which version should be used.

--- main_code/MPU6050.cpp
+++ main_code/MPU6050.cpp
@@ -3,20 +3,14 @@
  */
 
 #include "MPU6050.h"
-
-/**
- * @param frq the number of times per second update() will be called
- */
-MPU6050::MPU6050(int frq) {
-    frequency = frq;
-}
+#include<Wire.h>
+#include<Arduino.h>
 
 /**
  * Start communication with the unit, set its sensitivities.
  * This must be called when the Arduino is reset.
  */
 void MPU6050::init() {
-    Serial.println(frequency);
     Wire.begin();
     Wire.beginTransmission(ADDRESS);
     Wire.write(0x1B);   // Gyro register
@@ -37,26 +31,32 @@ void MPU6050::init() {
  * This method must be called on a regular basis to ensure accurate position calculations.
  * @param rad_vel must be double[3], is modified to contain {x,y,z} velocity data.
  * Values are in deg/s and range from [-2000, 2000]
+ * @param time micros() from the Arduino main loop
  */
-void MPU6050::update(double rad_vel[]) {
+void MPU6050::update(double rad_vel[], unsigned long time) {
     Wire.beginTransmission(ADDRESS);
     Wire.write(0x43);  // starting at register 0x43
     Wire.endTransmission(false);
     Wire.requestFrom(ADDRESS,6,true);  // requesting 6 registers
-    for (int i = 0; i < 6; i++) {
-        rad_vel[i] = Wire.read()<<8|Wire.read(); //reads two registers at a time for each dimension
-        rad_vel[i] *= DEGCONV;
-        gyro_pos[i] += rad_vel[i] / frequency;
+    unsigned long dt = time - last_update;
+    if (1000000.0 / dt < MAX_REFRESH) {
+        for (int i = 0; i < 3; i++) {
+            rad_vel[i] = Wire.read() << 8 | Wire.read(); //reads two registers at a time for each dimension
+            rad_vel[i] *= DEGCONV;
+            gyro_pos[i] += rad_vel[i] * dt / 1000000.0;
+        }
+        last_update = time;
     }
     Wire.endTransmission(true); //release i2c
 }
 
 /**
- * Identical to update(double[]), but does not require passing a variable.
+ * Identical to update(double[]), but does not require passing a velocity variable.
+ * @param time micros() from the Arduino main loop
  */
-void MPU6050::update() {
-    double x[3];
-    MPU6050::update(x);
+void MPU6050::update(unsigned long time) {
+    double y[3];
+    MPU6050::update(y, time);
 }
 
 /**
@@ -68,3 +68,5 @@ void MPU6050::pos(double pos[]) {
         pos[i] = gyro_pos[i];
     }
 }
+
+
--- main_code/MPU6050.h
+++ main_code/MPU6050.h
@@ -1,15 +1,13 @@
-#include<Wire.h>
 class MPU6050 {
 private:
     const double DEGCONV = 2000 / 32786.0;
     const int ADDRESS = 0x68;
+    const int MAX_REFRESH = 2000; //max refresh rate in Hz
     double gyro_pos[3];
-    double frequency;
+    unsigned long last_update = 0;
 public:
-    MPU6050(int frq);
     void init();
-    void update();
-    void update(double rad_vel[]);
+    void update(unsigned long);
+    void update(double rad_vel[], unsigned long);
     void pos(double pos[]);
 };
-
