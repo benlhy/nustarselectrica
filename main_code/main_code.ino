602e81b93cc0024e56a4dbadcc8f4e5592e92226
ben <limhanyangb@gmail.com>
2018-01-28 15:08:10 -0600

    update

--- main_code/main_code.ino
+++ main_code/main_code.ino
@@ -13,182 +13,127 @@
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+/* Set the delay between fresh samples */
+#define BNO055_SAMPLERATE_DELAY_MS (100)
 
 Adafruit_BMP280 bme; // I2C
 
-int curr_alt = 0;
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
+int curr_alt = 0;
+int prevE = 0;
+int prevEI = 0;
+int prevED = 0;
+int currE = 0;
+int controlu = 0;
+
+
+float orientX = 0;
+float orientY = 0;
+float orientZ = 0;
+
+float desiredX = 0;
+float desiredY = 0;
+float desiredZ = 0;
+
+int Kp=0,Kd=0,Ki=0;
+/////////////////////////// SENSOR TEAM   /////////////////////
+void sensor_init() {
+    if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  bno.setExtCrystalUse(true);
+  
+}
+
+void sensor_update() {
+  sensors_event_t event;
+  bno.getEvent(&event);
+  float collectX=0;
+  float collectY=0;
+  float collectZ=0;
+  // The data will be very noisy, so we have to apply a moving average
+  for (int i=0;i<10;i++){
+    sensors_event_t event;
+    bno.getEvent(&event);
+    collectX=event.orientation.x + collectX;
+    collectY=event.orientation.y + collectY;
+    collectZ= event.orientation.z + collectZ;
+    delay(5);
+  }
+  orientX = collectX/10;
+  orientY = collectY/10;
+  orientZ = collectZ/10;
+}
 ////////////////////////// MOTOR TEAM /////////////////////////
 void motor_init() {
-  pinMode(A9, INPUT); //motor direction input A
-  pinMode(A8, INPUT); //motor direction input B
-  pinMode(A7, PWM); //controls motor speed
+  pinMode(A9, OUTPUT); //motor direction input A
+  pinMode(A8, OUTPUT); //motor direction input B
+  pinMode(A7, 0); //controls motor speed
 }
 // update the pwm signal based on input from sensors
-void motor_calc (){
-  int prevEI;
-  float pE, iE, dE;
-  float dAvg[5] = {prevE, prevE, prevE, prevE, prevE};
-  int dAvgPoint = 0;
-  int waitTimeConstant = waitTime;
-  float initialVelocityThreshold = 180;
-  enum flightPlan_t { RESET, PID } plan = RESET;
-  int calculatePID() {
-      float u;
-      int dt = currentData.time - prevData.time;
-      float error = 0;
-      error = calculateError();
-    
-      // Scales pE
-      pE = error * kp;
-    
-      // calculates Moving average derivative
-      dAvg[dAvgPoint] = (error - prevE) / dt;
-      dAvgPoint++;
-      dAvgPoint = dAvgPoint % 5;
-      dE = 0;
-      for (int i = 0; i < 5; i++) {
-          dE += dAvg[i];
-      }
-      dE = dE / 5;
+void motor_update (){
 
-      // Sets prevE to be this eror
-      prevE = error;
-
-      // Sets prevEI to be running sum
-      prevEI += error * dt;
-
-
-      // Anti integrator
-      if (prevEI > 60 / ki) { 
-          prevEI = 60 / ki;
-      }
-      if (prevEI < -60 / ki) {
-          prevEI = -60 / ki;
-      }
-
-      // scales iE
-      iE = prevEI * ki;
-
-      // scales dE
-      dE = kd * dE;
-
-      // Sum of everything is the motor output
-      u = iE + dE + pE;
-
-      return u;
+  currE = desiredX - orientX;
+  prevED = prevE - currE;
+  prevEI = prevEI + currE;
+  // anti integrator
+  if (prevEI>60){
+    prevEI = 60;
   }
-
-  // Takes in power and turns it into a directional output
-  void outputMotor(int power) {
-      // Positive power is direction Pin HIGH.
-      if (power > 0) {
-          digitalWrite(directionPin, HIGH);
-          analogWrite(speedPin, abs(power));
-      }
-      else { // else its 0 or negative. Either way, direction is LOW
-          digitalWrite(directionPin, LOW);
-          analogWrite(speedPin, abs(power));
-      }
+  if (prevEI<-60){
+    prevEI = -60;
   }
-
-  // Calculates PID value and outputs to motor.
-  void callThemAll(uint32_t startTime) {
-     plan = PID;
-
-
-          // u is the number to output to the Motor
-          int u = 0;
-
-          // if the plan is to PID the controller
-          if (plan == PID) {
-              // gets PID calculations
-              u = calculatePID();
-          }
-              // Else if the plan is to Reset, we call the resetVelocity function
-          else{
-              // sets u to the velocity given by the resetVelocity function
-              u = resetVelocity();
-          }
-
-          // limits u to +- 255, though it should be fine without this
-
-          if ( u < -255) {
-              u = -255;
-          }
-          // Outputs that to the motor
-
-          if (u > 255) {
-              u = 255;
-          }
-          outputMotor(u);
-      }
+  controlu = Kp*currE+Kd*prevED+Ki*prevE;
+  prevE = currE;
+  if (controlu>255){
+    digitalWrite(A8,1);
+    digitalWrite(A9,0);
+    analogWrite(A7,255);
   }
-
-  // Generates a linear trajectory to reach the desired position.
-  float calculateError(void) {
-      // get time elasped, which is currentTime - launchTime - waitTime
-      int timeElapsed = currentData.time - launchTimestamp - waitTimeConstant;
-      if (timeElapsed < 0)
-      {
-          return 0;
-      }
-      // this is kinda like how far from the target we should be
-      //So this returns anywhere from 0.8 of the original turnLeft to 0;
-      float errorT = 0.8 * ogTurnLeft - (0.8 * ogTurnLeft * timeElapsed) / targetTime;
-
-      // we dont want this to keep going forever
-      if (((errorT < 0) && (ogTurnLeft > 0)) || ((errorT > 0) && (ogTurnLeft < 0))) {
-          errorT = 0;
-      }
-
-      // we take turnLeft - errorT to get a line going from 20% of turnLeft to 100% of turnLeft in targetTime
-      errorT = turnLeft - errorT;
-
-      return errorT;
+  else if ((controlu<0)&&(controlu>-255)){
+    digitalWrite(A8,0);
+    digitalWrite(A9,1);
+    analogWrite(A7,-controlu);
   }
-
-  void resetController(void) {
-      prevEI=0;
-
-      ogTurnLeft = abs(ogTurnLeft);
-      turnLeft = ogTurnLeft;
-      prevE = 0.2*turnLeft;
-      powerG=0;
-      isLaunchGyroSet = 0;
-      pE=0;
-      iE=0;
-      dE=0;
-      dAvgPoint = 0;
-      initalVelocityThreshold = 180;
-      plan = RESET;
+  else if (controlu<-255){
+    digitalWrite(A8,0);
+    digitalWrite(A9,1);
+    analogWrite(A7,255);
   }
+  else if (controlu>0&&controlu<255){
+    digitalWrite(A8,1);
+    digitalWrite(A9,0);
+    analogWrite(A7,controlu);
   }
-
-void motor_update() {
-int calculatePID();
-
-//outputs the PID output to the motor, sanitizing as necessary
-void outputMotor(int power);
-
-//Calls the other functions in sequence to get an output to the motor
-void callThemAll(uint32_t timestamp);
-
-//generates a linear trajectory error path for us to follow
-float calculateError();
-
-//resets controller
-void resetController();
+  else if(controlu==0){
+    digitalWrite(A8,0);
+    digitalWrite(A9,0);
+    analogWrite(A7,controlu);
+  }
 }
+
+
 
 ///////////////////////////////////////////////////////////////
 
 ///////////////////////// RADIO TEAM //////////////////////////
 void radio_init() {
   Serial2.begin(9600); // USE TX/RX 2 of Teensy 3.5
+  for (int i =0; i<10; i++){
+    Serial2.write("Radio check");
+  }
 }
 
-void update_radio() {
+void radio_update() {
+  char reply[20];
+  Serial2.write("Hello back 2 you.");
   
 }
 ///////////////////////////////////////////////////////////////
@@ -197,7 +142,7 @@ void update_radio() {
 int trig = 0;
 int led = 1;
 
-void cameraSetup() {
+void camera_setup() {
   // initialize digital pins as output
   pinMode(led, OUTPUT);
   pinMode(trig, OUTPUT);
@@ -206,47 +151,33 @@ void cameraSetup() {
   digitalWrite(trig, HIGH);
 
 }
+///////////////////////////////////////////////////////////////
 
-void cameraLoop() {
-  // set a single loop for the camera to run
-  digitalWrite(trig, LOW);
-  digitalWrite(led, LOW);
-
-  delay(150);
-
-  digitalWrite(trig, HIGH);
-  digitalWrite(led, HIGH);
-
-  delay(500);//how long before it stops recording video
-}
 
 /////////////////////////////////////////////////////////////
 void setup() {
   
   Serial.begin(9600);
-  
   delay(1000);
   Serial.println("Hello");
 
   radio_init();
-
+  camera_setup();
   sensor_init();
- 
   motor_init();
-  // some code to start
-  //init_all();
-  //screen_huh(); // check if display is on or off
-  //spin_parameters_huh(); // ask ground station if we want to set new spin parameters
-  //motor_test_huh(); // do we want a motor test?
+  
 }
 
 void loop() {
-  // keep on spinning
-    sensor_update();
-    motor_calc();
+
+
+  // Here we update the sensors
+    sensor_update();// delay for 50ms
+  // Based on the sensors, pass sensor data to motors
     motor_update();
-
-
+  // update groundstation
+    radio_update(); 
+ 
 }
 
 
